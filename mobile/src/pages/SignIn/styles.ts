b25eb261e6e77a91ae3b036251357ce35eb84b3e
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-05-10 21:53:01 -0300

    app structure

---
+++ mobile/src/pages/SignIn/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  margin: 64px 0 24px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  color: #f4efe8;
+`;
