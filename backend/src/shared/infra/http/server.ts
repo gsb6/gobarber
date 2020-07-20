9e0f5f1acb4350485bc0f00e8f576cfd8751aebf
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-07-20 20:35:32 -0300

    rate limiter

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -6,16 +6,18 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
-import routes from './routes';
-import uploadConfig from '@config/upload';
-
-import AppError from '@shared/errors/AppError';
-
 import '@shared/infra/typeorm';
 import '@shared/container';
 
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
+
+import rateLimiter from './middlewares/rateLimiter';
+import routes from './routes';
+
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
